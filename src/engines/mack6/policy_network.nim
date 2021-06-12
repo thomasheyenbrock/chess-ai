96f0b36947aa4d307a2ded4dff45f19d7ce91328
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2021-06-12 17:54:00 +0200

    exit early in training

--- src/engines/mack6/policy_network.nim
+++ src/engines/mack6/policy_network.nim
@@ -21,8 +21,9 @@ let optim = policy_network.optimizerSGD(learning_rate = 1e-4'f32)
 # ##################################################################
 # Training
 
-proc train_policy_network*(x: Variable, y: Tensor, epochs: int = 100) =
+proc train_policy_network*(x: Variable, y: Tensor, epochs: int = 200) =
     echo "Training value network for ", epochs, " epochs"
+    var prev_loss = 0'f
     for e in 1..epochs:
         let y_pred = policy_network.forward(x)
         let loss = y_pred.mse_loss(y)
@@ -31,6 +32,11 @@ proc train_policy_network*(x: Variable, y: Tensor, epochs: int = 100) =
 
         loss.backprop()
         optim.update()
+
+        if abs(loss.value[0] - prev_loss) < 0.000_001:
+            echo "Loss reduction less than 1e-6, end training early"
+            break
+        prev_loss = loss.value[0]
 
 
 # ##################################################################
--- src/engines/mack6/value_network.nim
+++ src/engines/mack6/value_network.nim
@@ -21,8 +21,9 @@ let optim = value_network.optimizerSGD(learning_rate = 1e-4'f32)
 # ##################################################################
 # Training
 
-proc train_value_network*(x: Variable, y: Tensor, epochs: int = 200) =
+proc train_value_network*(x: Variable, y: Tensor, epochs: int = 500) =
     echo "Training value network for ", epochs, " epochs"
+    var prev_loss = 0'f
     for e in 1..epochs:
         let y_pred = value_network.forward(x)
         let loss = y_pred.mse_loss(y)
@@ -31,6 +32,11 @@ proc train_value_network*(x: Variable, y: Tensor, epochs: int = 200) =
 
         loss.backprop()
         optim.update()
+
+        if abs(loss.value[0] - prev_loss) < 0.000_001:
+            echo "Loss reduction less than 1e-6, end training early"
+            break
+        prev_loss = loss.value[0]
 
 
 # ##################################################################

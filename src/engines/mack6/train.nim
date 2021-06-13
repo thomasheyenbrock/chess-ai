5aabdb175e8f995afaaadc8c4a8b6313c23a66e7
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2021-06-13 16:02:00 +0200

    train on all data

--- src/engines/mack6/train.nim
+++ src/engines/mack6/train.nim
@@ -30,38 +30,16 @@ for i in 1..paramCount():
         policy_input.add(splitted[0].replace("@[", "").replace("]", "").split(", ").map(parseAsFloat))
         policy_output.add(splitted[1].replace("@[", "").replace("]", "").split(", ").map(parseAsFloat))
 
-echo "Shuffling"
-for i in 1..value_input.len*4:
-    let index = rand(0..value_input.len-1)
-
-    let value_input_item = value_input[index]
-    value_input.del(index)
-    value_input.add(value_input_item)
-
-    let value_output_item = value_output[index]
-    value_output.del(index)
-    value_output.add(value_output_item)
-
-    let policy_input_item = policy_input[index]
-    policy_input.del(index)
-    policy_input.add(policy_input_item)
-
-    let policy_output_item = policy_output[index]
-    policy_output.del(index)
-    policy_output.add(policy_output_item)
-
-let data_points = if value_input.len < 1024: value_input.len else: 1024
-
 load_value_network()
 train_value_network(
-    value_network_ctx.variable(value_input[0..data_points].toTensor),
-    value_output[0..data_points].toTensor
+    value_network_ctx.variable(value_input.toTensor),
+    value_output.toTensor
 )
 save_value_network()
 
 load_policy_network()
 train_policy_network(
-    policy_network_ctx.variable(policy_input[0..data_points].toTensor),
-    policy_output[0..data_points].toTensor
+    policy_network_ctx.variable(policy_input.toTensor),
+    policy_output.toTensor
 )
 save_policy_network()

530a4802e33e5876d09a8e8cabf5cdd27ce80c64
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-02-03 23:58:20 +0100

    calculate nodes per second

--- src/engines/mack7/src/main.rs
+++ src/engines/mack7/src/main.rs
@@ -12,9 +12,18 @@ fn main() {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
         &c,
     );
-    for _ in 0..10 {
-        let now = Instant::now();
-        game.count_legal_moves(5, &c);
-        println!("{}", now.elapsed().as_millis());
+    let now = Instant::now();
+    let mut nodes = 0;
+    for depth in 1..7 {
+        let moves = game.count_legal_moves(depth, &c);
+        nodes += moves;
+        println!("Depth {}: {} moves", depth, moves);
     }
+    let time = now.elapsed().as_nanos();
+    let seconds = time as f64 / 1_000_000_000_f64;
+    println!("");
+    println!("Seconds: {}", seconds);
+    println!("Nodes: {}", nodes);
+    println!("NPS: {}", nodes / seconds as u64);
+    // To beat: 12_413_253
 }

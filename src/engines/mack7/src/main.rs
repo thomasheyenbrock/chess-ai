296b34f4e8aee0aacd03ebc4d98e617f3dff112f
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-02-04 09:52:37 +0100

    average nps

--- src/engines/mack7/src/main.rs
+++ src/engines/mack7/src/main.rs
@@ -12,18 +12,28 @@ fn main() {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
         &c,
     );
-    let now = Instant::now();
-    let mut nodes = 0;
-    for depth in 1..7 {
-        let moves = game.count_legal_moves(depth, &c);
-        nodes += moves;
-        println!("Depth {}: {} moves", depth, moves);
+
+    let runs = 10;
+    let mut total_nps = 0.0;
+    for _ in 0..runs {
+        let now = Instant::now();
+        let mut nodes = 0;
+        for depth in 1..7 {
+            let moves = game.count_legal_moves(depth, &c);
+            nodes += moves;
+            println!("Depth {}: {} moves", depth, moves);
+        }
+        let time = now.elapsed().as_nanos();
+        let seconds = time as f64 / 1_000_000_000_f64;
+        let nps = nodes as f64 / seconds;
+        total_nps += nps;
+        println!("");
+        println!("Seconds: {}", seconds);
+        println!("Nodes: {}", nodes);
+        println!("NPS: {}", nps as u64);
+        println!("\n");
     }
-    let time = now.elapsed().as_nanos();
-    let seconds = time as f64 / 1_000_000_000_f64;
-    println!("");
-    println!("Seconds: {}", seconds);
-    println!("Nodes: {}", nodes);
-    println!("NPS: {}", nodes / seconds as u64);
+
+    println!("Average NPS: {}", (total_nps / runs as f64) as u64);
     // To beat: 12_413_253
 }
